5c65f118bdd3f6ef6412c7e67a74c8caf3a249ee
ArtemE <qgren@ukr.net>
2022-09-16 19:34:08 +0300

    completed Task6

---
+++ Dz/Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введи число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0){
+    Console.Write($"{a} - чётное число ");
+}
+else{
+    Console.Write($"{a} - нечётное число ");
+}
