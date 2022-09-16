23191ba3e7aca6e5510687f33b8252bc217924da
ArtemE <qgren@ukr.net>
2022-09-16 19:12:31 +0300

    comleted Task1 "

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Write("Введи первое число: ");
+int a = Convert.ToInt32(ReadLine());
+Write("Введи второе число: ");
+int b = Convert.ToInt32(ReadLine());
+int c = b ^ 2;
+WriteLine(c);
+if (b * b == a){
+    WriteLine($"Число {a} является квадратом числа {b}");
+}
+else{
+    WriteLine($"Число {a} не является квадратом числа {b}");
+}
