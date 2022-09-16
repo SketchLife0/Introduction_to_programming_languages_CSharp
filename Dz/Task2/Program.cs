3d10986604faa9d4d035f122b7abfcfddd9f3345
ArtemE <qgren@ukr.net>
2022-09-16 19:14:12 +0300

    create folder DZ

---
+++ Dz/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+Write("Введи первое число: ");
+int a = Convert.ToInt32(ReadLine());
+Write("Введи Второе число: ");
+int b = Convert.ToInt32(ReadLine());
+if (a > b){
+    Write($"max = {a}");
+}
+else{
+    Write($"max = {b}");
+}
