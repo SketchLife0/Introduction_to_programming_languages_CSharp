53a50a707651e00f90ac87b2d4712e88f34ce34c
ArtemE <qgren@ukr.net>
2022-09-16 19:27:46 +0300

    copleted Task4

---
+++ Dz/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b){
+    if (a > c){
+        Console.WriteLine($"max = {a}");
+    }
+    else{
+        Console.WriteLine($"max = {c}");
+    }
+}
+else{
+    if (b > c){
+        Console.WriteLine($"max = {b}");
+    }
+    else{
+        Console.WriteLine($"max = {c}");
+    }
+}
