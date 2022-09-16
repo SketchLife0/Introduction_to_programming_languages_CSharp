29f93dd849607d9bdded7d601b1e61a3969889b5
Artem Eremenko <qgren@ukr.net>
2022-09-17 00:22:49 +0300

    completed Task8

---
+++ Dz/Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+while(b <= a){
+    if (b % 2 == 0){
+        Console.Write($"{b} ");
+    }
+    b++;
+}
