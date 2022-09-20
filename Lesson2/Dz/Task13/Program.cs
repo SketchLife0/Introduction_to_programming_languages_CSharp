bb9464e81bfb75cd659b39fd49e53ae1edaf3bd6
ArtemE <qgren@ukr.net>
2022-09-20 17:24:15 +0300

    comleted task13

---
+++ Lesson2/Dz/Task13/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num > 999) num = num / 10;
+if (num < 100) Console.Write("Третьего числа нет.");
+else {num = num % 10;
+     Console.Write(num);
+}
