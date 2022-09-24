b77f35d5f5e55c83d626a3496a07748e85bc3cbf
Artem Eremenko <qgren@ukr.net>
2022-09-25 00:36:16 +0300

    Completed Task21

---
+++ Lesson2/Seminar3/Dz/Task21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите Х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2)+Math.Pow(z1-z2, 2));
+Console.WriteLine(result);
