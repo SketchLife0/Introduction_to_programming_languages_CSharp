8f7f5184dfe3568e1dbdd7405bde2a03b14ec5f6
Artem Eremenko <qgren@ukr.net>
2022-09-24 19:38:55 +0300

    Completed Task19

---
+++ Lesson2/Seminar3/Dz/Task19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num > 99999 | num < 10000)
+{
+    Console.Write("Дурачок, введи нормально: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+int num2 = num / 1000 % 10;
+int num4 = num / 10 % 10;
+if (num / 10000 == num % 10 && num2 == num4) Console.WriteLine("Кайф");
+else Console.WriteLine("Не кайф");
