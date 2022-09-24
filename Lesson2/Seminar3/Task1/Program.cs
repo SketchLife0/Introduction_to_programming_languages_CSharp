60226b202de6545cd861ada698fb57fa92f6619c
Artem Eremenko <qgren@ukr.net>
2022-09-25 00:39:34 +0300

    Classwork Task1

---
+++ Lesson2/Seminar3/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch(num)
+{
+    case 1:
+    {
+        Console.WriteLine("X<0 Y>0");
+        break;
+    }
+    case 2:{
+        Console.WriteLine("X>0 Y>0");
+        break;
+    }
+    case 3:{
+        Console.WriteLine("X<0 Y<0");
+        break;
+    }
+    case 4:{
+        Console.WriteLine("X<0 Y<0");
+        break;
+    }
+}
