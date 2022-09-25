a37e735c7acc83f42172278fafa386dfa139b73f
Artem Eremenko <qgren@ukr.net>
2022-09-25 12:17:45 +0300

    addition to the task19

--- Lesson2/Seminar3/Dz/Task19/Program.cs
+++ Lesson2/Seminar3/Dz/Task19/Program.cs
@@ -1,11 +1,25 @@
-﻿Console.Write("Введите пятизначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-while (num > 99999 | num < 10000)
+﻿// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+// Console.Write("Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// while (num > 99999 | num < 10000)
+// {
+//     Console.Write("Дурачок, введи нормально: ");
+//     num = Convert.ToInt32(Console.ReadLine());
+// }
+// int num2 = num / 1000 % 10;
+// int num4 = num / 10 % 10;
+// if (num / 10000 == num % 10 && num2 == num4) Console.WriteLine("Кайф");
+// else Console.WriteLine("Не кайф");
+
+// Второй вариант для любого числа
+Console.Write("Введите число: ");
+string a = Console.ReadLine()!;
+int num = Convert.ToInt32(a);
+string result = "";
+while (num != 0)
 {
-    Console.Write("Дурачок, введи нормально: ");
-    num = Convert.ToInt32(Console.ReadLine());
+    result += Convert.ToString(num % 10);
+    num/=10;
 }
-int num2 = num / 1000 % 10;
-int num4 = num / 10 % 10;
-if (num / 10000 == num % 10 && num2 == num4) Console.WriteLine("Кайф");
+if (result == a) Console.WriteLine("Кайф");
 else Console.WriteLine("Не кайф");
