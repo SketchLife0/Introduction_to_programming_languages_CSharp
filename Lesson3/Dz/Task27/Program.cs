9837514164c1b4279c6597ba2fca45917f8d1c9d
ArtemE <qgren@ukr.net>
2022-09-27 17:34:10 +0300

    Comleted Task27

---
+++ Lesson3/Dz/Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+int NumberOfDigits(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberOfDigits(a));
