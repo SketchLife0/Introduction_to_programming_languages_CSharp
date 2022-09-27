44cd676e53acef5e159e7433c212ace5e9077664
ArtemE <qgren@ukr.net>
2022-09-27 17:19:28 +0300

    Coplete Task1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int NumberOfDigits(int num){
+    int result = 1;
+    while (num > 9) {
+        num /= 10;
+        result++;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberOfDigits(a));
