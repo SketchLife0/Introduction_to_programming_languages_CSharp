25665fa35c0e3fb99b4254c08616860120206d2c
ArtemE <qgren@ukr.net>
2022-09-28 21:49:02 +0300

    Completed Task29

---
+++ Lesson3/Dz/Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите метод, который формирует массив из 8 случайных элементов и выводит их на экран.
+void ArrayCreation(int lenght)
+{
+    int[] list = new int[lenght];
+    Console.Write("[");
+    for (int i = 0; i < lenght; i++)
+    {
+        list[i] = new Random().Next(1, 101);
+        if (i != lenght - 1) Console.Write($"{list[i]}, ");
+        else Console.Write($"{list[i]}]");
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+ArrayCreation(a);
