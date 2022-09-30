c529188263620d19aa7dfec74b8f8362ebbe9cb1
Artem Eremenko <qgren@ukr.net>
2022-09-30 22:23:02 +0300

    Beauty correction Task34

--- Lesson3/Seminar5/Dz/Task34/Program.cs
+++ Lesson3/Seminar5/Dz/Task34/Program.cs
@@ -1,9 +1,13 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+void EvenNumbersInArray (int num){
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++) array[i] = new Random().Next(100, 1000);
+    int result = 0;
+    for (int i = 0; i < num; i++) if (array[i] % 2 == 0) result += 1;
+    Console.WriteLine(String.Join(",", array));
+    Console.WriteLine(result);
+}
+
 Console.Write("Введите размер массива: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[a];
-for (int i = 0; i < a; i++) array[i] = new Random().Next(100, 1000);
-int result = 0;
-for (int i = 0; i < a; i++) if (array[i] % 2 == 0) result += 1;
-Console.WriteLine(String.Join(",", array));
-Console.WriteLine(result);
+EvenNumbersInArray(a);
