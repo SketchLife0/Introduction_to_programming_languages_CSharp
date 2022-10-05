0e97ef8244dbb5f357f7eca0c1f5e714cb5e4224
Artem Eremenko <qgren@ukr.net>
2022-10-05 18:11:50 +0300

    Completed Task41

---
+++ Lesson4/Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int NumCalculation(string strnum)
+{
+    string[] arNum = strnum.Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+// Если убрать ',' а 9 строку изменить на double a = Convert.ToDouble(item) то можно спокойно работать и с числами с плавающей точкой
+    int result = 0;
+    foreach (var item in arNum)
+    {
+        int a = Convert.ToInt32(item);
+        if (a > 0) result++;
+    }
+    return result;
+}
+
+
+Console.Write("Введите целые числа: ");
+string a = Console.ReadLine()!;
+Console.WriteLine(NumCalculation(a));
