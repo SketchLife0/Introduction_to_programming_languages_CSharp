de116bc0328e170cda7c4be03c46006d384792d6
Artem Eremenko <qgren@ukr.net>
2022-10-16 01:03:05 +0300

    completed Task66

---
+++ FinalProject/Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// В решении задач ОБЯЗАТЕЛЬНОЕ использование рекурсии!
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Recalculation(int start, int exit){ 
+    return start < exit ? start + Recalculation(++start, exit) : start;
+}
+
+Console.Write("Введите число начала: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число конца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write(Recalculation(a, b));
